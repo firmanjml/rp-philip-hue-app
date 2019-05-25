bb78bcd018822478118916c872de889ce9e515e2
Iman Rizal <17040236@myrp.edu.sg>
2019-05-25 23:17:39 +0800

    Added Convertion from XY to HEX code

--- lighue/components/ColorConvert.js
+++ lighue/components/ColorConvert.js
@@ -35,12 +35,47 @@ export const ColorConversionToXY = (color) => {
     if (isNaN(y)) {
         y = 0;
     }
-    // console.log("X " + x);
-    // console.log("Y " + y);
-
-    // return {x,y}
     
     return [parseFloat(x),parseFloat(y)]
+}
+
+export const ConvertXYtoHex = (x, y, bri) => {
+    z = 1.0 - x - y;
+
+    Y = bri / 255.0; // Brightness of lamp
+    X = (Y / y) * x;
+    Z = (Y / y) * z;
+
+    r = X * 1.612 - Y * 0.203 - Z * 0.302;
+    g = -X * 0.509 + Y * 1.412 + Z * 0.066;
+    b = X * 0.026 - Y * 0.072 + Z * 0.962;
+
+    r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
+    g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
+    b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
+
+    maxValue = Math.max(r, g, b);
+    r /= maxValue;
+    g /= maxValue;
+    b /= maxValue;
+
+    r = r * 255; if (r < 0) { r = 255 };
+    g = g * 255; if (g < 0) { g = 255 };
+    b = b * 255; if (b < 0) { b = 255 };
+
+    r = Math.round(r).toString(16);
+    g = Math.round(g).toString(16);
+    b = Math.round(b).toString(16);
+
+    if (r.length < 2)
+        r = "0" + r;
+    if (g.length < 2)
+        g = "0" + g;
+    if (b.length < 2)
+        b = "0" + r;
+    rgb = "#" + r + g + b;
+
+    return rgb;
 }
 
 
