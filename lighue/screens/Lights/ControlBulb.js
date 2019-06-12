f8cca121c16c646f5956f1e90fa5593f9c63ca86
ImanRizal99 <46244162+ImanRizal99@users.noreply.github.com>
2019-06-12 18:31:40 +0800

    Fix issue #3

--- lighue/screens/Lights/ControlBulb.js
+++ lighue/screens/Lights/ControlBulb.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Image} from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
 import { ColorPicker } from 'react-native-color-picker'
 
 import { connect } from 'react-redux'
@@ -34,13 +34,14 @@ class ControlBulb extends React.Component {
         id: null,
         sat: null,
         bri: null,
+        dimmableType: null,
         satPer: null,
         briPer: null,
         roomName: "None"
     };
 
     componentWillMount() {
-        this.setState({ id: this.props.navigation.getParam('id', 'NO-ID') })
+        this.setState({ id: this.props.navigation.getParam('id', 1) })
     }
 
     componentDidMount() {
@@ -48,6 +49,7 @@ class ControlBulb extends React.Component {
         this.calculatePercentage("sat", this.props.lights[this.state.id].state.sat)
         this.setState({
             sat: this.props.lights[this.state.id].state.sat,
+            dimmableType: this.props.lights[this.state.id].type != "Dimmable light" ? false : true,
             bri: this.props.lights[this.state.id].state.bri
         })
     }
@@ -61,12 +63,12 @@ class ControlBulb extends React.Component {
                 on: true
             });
         }
-            axios({
-                method: 'PUT',
-                url: `http://${bridgeip[bridgeIndex]}/api/${username[bridgeIndex]}/lights/${id}/state`,
-                data: { xy: ColorConversionToXY(values) }
-            })
-        }
+        axios({
+            method: 'PUT',
+            url: `http://${bridgeip[bridgeIndex]}/api/${username[bridgeIndex]}/lights/${id}/state`,
+            data: { xy: ColorConversionToXY(values) }
+        })
+    }
 
     changeSatLightState = (values) => {
         const { bridgeip, username, bridgeIndex, lights, _changeLampStateByID } = this.props;
@@ -106,18 +108,18 @@ class ControlBulb extends React.Component {
         const result = Math.round((values * 100) / 254);
         if (result == 0) {
             if (arg == "sat") {
-                this.setState({ satPer: 1 })
+                this.setState({ satPer: 1 + "%" })
             }
             else {
-                this.setState({ briPer: 1 })
+                this.setState({ briPer: 1 + "%" })
             }
         }
         else {
             if (arg == "sat") {
-                this.setState({ satPer: result })
+                this.setState({ satPer: result + "%" })
             }
             else {
-                this.setState({ briPer: result })
+                this.setState({ briPer: result + "%" })
             }
         }
     }
@@ -125,57 +127,49 @@ class ControlBulb extends React.Component {
     onLights = (boolean) => {
         const { id } = this.state;
         this.props._changeLampStateByID(id, {
-            on:boolean
+            on: boolean
         })
     }
 
-    renderBriSlider() {
+    renderControl() {
         const { nightmode } = this.props;
-        const { bri } = this.state;
+        const { dimmableType, bri, sat, briPer, satPer } = this.state;
+        const { colors } = theme;
         const trackTintColor = nightmode ? "rgba(157, 163, 180, 0.10)" : "#DDDDDD"
+        const textcolor = { color: nightmode ? colors.white : colors.gray3 }
+        const control = dimmableType ? ["Brightness"] : ["Brightness", "Saturation"]
         return (
-            <Slider
-                minimumValue={1}
-                maximumValue={254}
-                style={{ height: 19 }}
-                thumbStyle={styles.thumb}
-                trackStyle={{ height: 10, borderRadius: 10 }}
-                minimumTrackTintColor={theme.colors.secondary}
-                maximumTrackTintColor={trackTintColor}
-                value={bri}
-                onValueChange={this.changeBriLightState}
-            />
-        )
-    }
-
-    renderSatSlider() {
-        const { nightmode } = this.props;
-        const trackTintColor = nightmode ? "rgba(157, 163, 180, 0.10)" : "#DDDDDD"
-        return (
-            <Slider
-                minimumValue={1}
-                maximumValue={254}
-                style={{ height: 19 }}
-                thumbStyle={styles.thumb}
-                trackStyle={{ height: 10, borderRadius: 10 }}
-                minimumTrackTintColor={theme.colors.secondary}
-                maximumTrackTintColor={trackTintColor}
-                value={this.state.sat}
-                onValueChange={this.changeSatLightState}
-            />
-        )
+            Object.keys(control).map(val => (
+                <View>
+                    <Text style={[styles.textControl, textcolor, { marginBottom: 10 }]}>{control[val]}</Text>
+                    <Slider
+                        minimumValue={1}
+                        maximumValue={254}
+                        style={{ height: 19 }}
+                        thumbStyle={styles.thumb}
+                        trackStyle={{ height: 10, borderRadius: 10 }}
+                        minimumTrackTintColor={theme.colors.secondary}
+                        maximumTrackTintColor={trackTintColor}
+                        value={control[val] == "Brightness" ? bri : sat}
+                        onValueChange={control[val] == "Brightness" ? this.changeBriLightState : this.changeSatLightState}
+                    />
+                    <Text style={[styles.textPer, textcolor]}>{control[val] == "Brightness" ? briPer : satPer}</Text>
+                </View>
+            )))
     }
 
     renderColorPicker() {
-        return (
-            <ColorPicker
-                onColorChange={this.changeColorLightState}
-                style={{ flex: 1 }}
-                hideSliders={true}
-            // color={ConvertXYtoHex(this.props.lights[this.state.id].state.xy[0], this.props.lights[this.state.id].state.xy[1], 254)}
-            // color={this.state.color}
-            />
-        )
+        const { dimmableType } = this.state;
+        if (!dimmableType) {
+            return (
+                <ColorPicker
+                    onColorChange={this.changeColorLightState}
+                    style={{ flex: 1 }}
+                    hideSliders={true}
+                    color={dimmableType ? "black" : null}
+                />
+            )
+        }
     }
 
     renderToggleButton() {
@@ -192,7 +186,7 @@ class ControlBulb extends React.Component {
     }
 
     render() {
-        const { nightmode } = this.props;
+        const { nightmode, lights } = this.props;
         const { colors } = theme;
         const backgroundcolor = { backgroundColor: nightmode ? colors.background : colors.backgroundLight }
         const titlecolor = { color: nightmode ? colors.white : colors.black }
@@ -202,7 +196,7 @@ class ControlBulb extends React.Component {
             <Block style={backgroundcolor}>
                 <Block container>
                     <View style={styles.titleRow}>
-                        <Text style={[styles.title, titlecolor]}>{this.props.lights[this.state.id].name}</Text>
+                        <Text style={[styles.title, titlecolor]}>{lights[this.state.id].name}</Text>
                         {this.renderToggleButton()}
                     </View>
                     <Text style={[styles.textControl, textcolor]}>Room Name</Text>
@@ -212,12 +206,7 @@ class ControlBulb extends React.Component {
                         value={this.state.roomName}
                         placeholderTextColor={nightmode ? colors.gray2 : colors.black}
                     />
-                    <Text style={[styles.textControl, textcolor, { marginBottom: 10 }]}>Brightness</Text>
-                    {this.renderBriSlider()}
-                    <Text style={[styles.textPer, textcolor]}>{this.state.briPer}%</Text>
-                    <Text style={[styles.textControl, textcolor, { marginBottom: 10 }]}>Saturation</Text>
-                    {this.renderSatSlider()}
-                    <Text style={[styles.textPer, textcolor]}>{this.state.satPer}%</Text>
+                    {this.renderControl()}
                     {this.renderColorPicker()}
                 </Block>
             </Block >
